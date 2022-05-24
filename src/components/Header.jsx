54e04956bfc6ba55b03b279b84c545919b1d469c
Avichez <starkavichi@gmail.com>
2022-05-24 20:33:48 +0300

    add gh-pages and link to repo in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <div className="nav-wrapper light-blue darken-4">
                     <a href="!#" className="brand-logo yellow-text text-accent-4">FORTNITE SHOP</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="!#">Sass</a></li>
+                        <li><a href="https://avichez.github.io/fortnite-shop/">Repository</a></li>
                     </ul>
                 </div>
             </nav>
